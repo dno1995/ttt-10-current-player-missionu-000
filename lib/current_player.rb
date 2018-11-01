3d431af69b09f11da16d74755636b72623414fec
Dane Olsen <dno1995@gmail.com>
2018-11-01 13:14:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,9 +4,16 @@ def turn_count(board)
     if spot != ' ' && spot != ''
       counter += 1
     end
-  counter
   end
+  counter
 end
 
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-turn_count(board)
+def current_player(board)
+  turn_count = turn_count(board) 
+  if turn_count % 2 == 0
+    current_player = 'X'
+  else
+    current_player = 'O'
+  end
+  current_player
+end
